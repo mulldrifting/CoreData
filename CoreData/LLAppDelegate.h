a10548cc346374aaf000070704d6d4dc9e7689ef
Lauren Lee <unrealeel@gmail.com>
2014-05-07 09:25:31 -0700

    Initial Commit

---
+++ CoreData/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  CoreData
+//
+//  Created by Lauren Lee on 5/7/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreData/LLViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLViewController.h
+//  CoreData
+//
+//  Created by Lauren Lee on 5/7/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLViewController : UIViewController
+
+@end
